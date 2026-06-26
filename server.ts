e43dbaaba4346dfd7c5cc986586db6334da0ff6a
geibbymeyrith-prog <geibbymeyrith@gmail.com>
2026-06-26 13:39:38 +0700

    Railway compatibility improvements

--- server.ts
+++ server.ts
@@ -45,7 +45,7 @@ const __dirname = process.cwd();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json({
     verify: (req: any, res, buf) => {
